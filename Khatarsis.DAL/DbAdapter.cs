562c708cb3ac7af314a8a9aa234d0b812e1d10e3
fbars <fbarszczewski@outlook.com>
2020-01-31 17:11:01 +0100

    Dodaj pliki projektów.

---
+++ Khatarsis.DAL/DbAdapter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient; 
+
+namespace Khatarsis.DAL
+{
+    public class DbAdapter
+    {
+        private const string connString = "SERVER = remotemysql.com" + ";USERID= IalS35jGSf" + ";PASSWORD= lHxoGp4AQC" +
+                                  ";DATABASE= IalS35jGSf" + ";Connection Timeout=3;";
+
+        //create new record in mysql database to reserve id
+        private void InsertNewBlankRma()
+        {
+
+            var con = new MySqlConnection(connString);
+
+            var cmd = new MySqlCommand();
+            cmd.Connection = con;
+
+            cmd.CommandText = "INSERT INTO rma (id) VALUES (NULL)";
+
+            try
+            {
+            con.Open();
+            cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("InsertNewBlankRma Error\n\n"+e.Message);
+            }
+            finally { con.Close(); }
+        }
+
+        //returns last RMA id
+        private void SelectLastRmaId()
+        {
+
+        }
+
+
+
+    }
+}
